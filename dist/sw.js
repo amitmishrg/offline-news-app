874c798edc8742821db00297881290b4e66f9ee1
Amit Mishra <amituidev>
2019-01-04 19:43:48 +0530

    pwa build news app

--- dist/sw.js
+++ dist/sw.js
@@ -1,3 +1,5 @@
+importScripts("precache-manifest.f49cb66739ed98c03dde2e2494c6c05b.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
 
 if (workbox) {
