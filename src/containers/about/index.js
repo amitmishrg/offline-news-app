a1530dd2a1ffc0485a2402252c8ffedf5be2a741
Amit Mishra <amituidev>
2019-01-28 19:31:27 +0530

    about page

--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -4,7 +4,8 @@ import style from './index.css';
 
  const About = () => (
         <div className="about-section">
-            <h1>About page Section</h1>
+            <h1>Offline News Web-App</h1>
+            <p>Offline News Web App build with React.js, React Router, Webpack4, Workbox, Service Worker, PWA, Intersection-observer</p>
         </div>
     )
 
